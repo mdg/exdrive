a3500df368e108f5de98b4af7b218f1be2ff54ab
Matthew Graham <matt.graham@teacherspayteachers.com>
2016-09-28 00:25:27 +0000

    some probably wrong progress on distributor

--- lib/exdrive/distributor.ex
+++ lib/exdrive/distributor.ex
@@ -1,12 +1,22 @@
 defmodule Exdrive.Distributor do
     use Application
 
-    def start(_type) do
-        GenServer.start_link(__MODULE__, :ok, [])
-    end
+    def start(_type, _args) do
+        import Supervisor.Spec, warn: false
 
-    def init(:ok) do
-        {:ok, %{}}
+        children = [
+            # Start the endpoint when the application starts
+            supervisor(Exdrive.Distributor.Server, []),
+            # Here you could define other workers and supervisors as children
+            # worker(Exdrive.Worker, [arg1, arg2, arg3]),
+        ]
+
+        # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
+        # for other strategies and supported options
+        opts = [strategy: :one_for_one, name: Exdrive.Supervisor]
+        Supervisor.start_link(children, opts)
+        Exdrive.Distributor.Supervisor.start_link()
+        Supervisor.start_link(children, opts)
     end
 
 end
